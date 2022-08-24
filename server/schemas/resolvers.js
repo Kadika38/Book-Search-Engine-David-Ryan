e1ffbb8b621ea8bb0a8b110c7777015974c9601c
Kadika <dryan10101@gmail.com>
2022-08-24 13:41:26 -0700

    updated resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,11 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        user: async (parent, { username }) => {
-            return User.findOne({ username }).populate('savedBooks');
+        me: async (parent, {  }, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('savedBooks');
+            }
+            throw new AuthenticationError('You need to be logged in!');
         },
 
     },
